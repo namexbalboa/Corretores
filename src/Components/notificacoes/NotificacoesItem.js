97da1537dcb656cbf46a1f874ce134bd907b4eda
Namex <naama.ortiz@gmail.com>
2018-12-10 02:59:04 -0400

    antes do modal

--- src/Components/notificacoes/NotificacoesItem.js
+++ src/Components/notificacoes/NotificacoesItem.js
@@ -1,135 +1,36 @@
 import React, { Component } from 'react';
 import {StyleSheet, Animated, ScrollView, TouchableHighlight, View, Text, Image, TouchableOpacity} from 'react-native';
-//import Modal from "react-native-modal";
+import Modal from "react-native-modal";
 
 export default class NotificacoesItem extends Component{
     
     constructor(props){
         super(props);
         this.state = { 
-            //isModalVisible: false,
+            isModalVisible: false,
         }
     }
 
-    /*
-    static navigationOptions = {
-        title: '',
-        tabBarLabel:'Busca',
-        header:null
-    }
-    */
-
-    /*MODAL
+ 
     _toggleModal = () =>
     this.setState({ isModalVisible: !this.state.isModalVisible });
-    //MODAL
+
     renderModalContent = () => (
         <ScrollView>
         <View style={stylesItem.modalContent}>  
-            
-                <View style={stylesItem.modalHeader}>   
-                    <Text style={{fontSize: 20, fontWeight: 'bold'}}>{this.props.data.name}</Text>
-                    <TouchableHighlight style={stylesItem.modalFechar} onPress={this._toggleModal}>
-                        <Text style={{fontSize:20, color: 'white'}}>X</Text>
-                    </TouchableHighlight>
-                </View>
-
-
-                <View style={stylesItem.modalTop}>      
-            
-                    <Image source={require('../../../assets/img/bg4.jpg')} style={stylesItem.img}/>
-
-
-
-                    <View style={stylesItem.modalBtns}>
-                        <TouchableHighlight underlayColor={'green'} style={{backgroundColor: 'lightgreen', padding:12, width: '50%', justifyContent: 'space-around', alignItems: 'center'}} onPress={this.props.Button}>
-                            <Text style={{fontSize:20, color: 'white'}}>Visitar</Text>
-                        </TouchableHighlight>
-                        <TouchableHighlight underlayColor={'pink'} style={{backgroundColor: 'purple', padding:12, width: '50%', justifyContent: 'space-around',  alignItems: 'center'}} onPress={this.Button}>
-                            <Text style={{fontSize:20, color: 'white'}}>Negociar</Text>
-                        </TouchableHighlight>
-                    </View> 
-
-                    <View style={{paddingTop: 15}}>
-                        <Text style={{fontSize:15, fontWeight:'bold', color: 'gray'}}>Lorem Ipsum is simply</Text>
-                        <Text style={{color:'gray', textAlign: 'justify'}}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                        when an unknown printer took a galley of type and scrambled it to make a type 
-                        specimen book.
-                        </Text>
-                    </View>
-
-                    <View style={stylesItem.modalCard}> 
-
-                        <View style={{flexDirection:'column', alignItems:'center'}}>
-                            <Image source={require('../../../assets/img/icons/home.png')} style={stylesItem.icon}/>
-                            <Text style={[stylesItem.txtBtn, {paddingTop:3}]}>Valor:</Text>
-                        </View>
-
-                        <Text style={[stylesItem.txtBtn,{marginLeft: 25}]}>R$ 1.000.000,00</Text>
-
-                    </View>
-
-                    <View style={stylesItem.modalCard}>  
-
-                        <View style={{flexDirection:'column', alignItems:'center'}}>
-                            <Image source={require('../../../assets/img/icons/home.png')} style={stylesItem.icon}/>
-                            <Text style={[stylesItem.txtBtn, {paddingTop:3}]}>Quartos:</Text>
-                        </View>
-
-                        <Text style={[stylesItem.txtBtn,{marginLeft: 25}]}>{this.props.data.quartos} (1 Suíte)</Text>
-
-                    </View>
-
-                    <View style={stylesItem.modalCard}> 
-
-                        <View style={{flexDirection:'column', alignItems:'center'}}>
-                            <Image source={require('../../../assets/img/icons/home.png')} style={stylesItem.icon}/>
-                            <Text style={[stylesItem.txtBtn, {paddingTop:3}]}>Metros:</Text>
-                        </View>
-
-                        <Text style={[stylesItem.txtBtn,{marginLeft: 25}]}>{this.props.data.metros}m²</Text>
-
-                    </View>
-
-                    <View style={stylesItem.modalCard}> 
-
-                        <View style={{flexDirection:'column', alignItems:'center'}}>
-                            <Image source={require('../../../assets/img/icons/home.png')} style={stylesItem.icon}/>
-                            <Text style={[stylesItem.txtBtn, {paddingTop:3}]}>Vagas:</Text>
-                        </View>
-
-                        <Text style={[stylesItem.txtBtn,{marginLeft: 25}]}>{this.props.data.vagas} carros</Text>
-
-                    </View>
-
-                    <View style={stylesItem.modalCard}> 
-
-                        <View style={{flexDirection:'column', alignItems:'center'}}>
-                            <Image source={require('../../../assets/img/icons/home.png')} style={stylesItem.icon}/>
-                            <Text style={[stylesItem.txtBtn, {paddingTop:3}]}>Valor:</Text>
-                        </View>
-
-                        <Text style={[stylesItem.txtBtn,{marginLeft: 25}]}>R$ 1.000.000,00</Text>
-
-                    </View>
-
-                </View>
-
-                <View style={stylesItem.modalBottom}>  
-                    <TouchableOpacity onPress={this._toggleModal} style={stylesItem.btnFechar}>
-                    <Text style={stylesItem.txtbtnFechar}>Fechar</Text>
-                    </TouchableOpacity>
-                </View>
+                <Text>Hello</Text>
         </View>
         </ScrollView>
     );
 
+    houseModalClick(){
+    }
 
+    render() {
+        return (
+                <View style={stylesItem.container}>
 
-    //DENTRO DO VIEW
-      <Modal 
+                        <Modal 
                         isVisible={this.state.isModalVisible}
                         backdropOpacity={0.7}
                         animationInTiming={800}
@@ -143,71 +44,33 @@ export default class NotificacoesItem extends Component{
                             </View>
                         </Modal>
 
-        */
-
-    houseModalClick(){
-    }
-
-    render() {
-        return (
-                <View style={stylesItem.viewItem}>
-
-                    <View style={stylesItem.viewTop}>
                         <TouchableHighlight underlayColor="#DDDDDD" onPress={this._toggleModal}>
-                            <Text style={stylesItem.txtBtn}>Teste1</Text>
-                        </TouchableHighlight>
+                        <View>
 
-                        <TouchableHighlight underlayColor="red" onPress={this.houseModalClick}>
-                            <Image source={require('../../../assets/img/icons/favorito.png')} style={stylesItem.icon}/>
-                        </TouchableHighlight>
-                        
-                    </View>
-
-                    <View style={stylesItem.viewBottom}>
-                        <TouchableHighlight underlayColor="#DDDDDD" style={stylesItem.buttonArea} onPress={this._toggleModal}>
+                            <View style={stylesItem.linha}>
                             
-                            <View style={stylesItem.AreaItens}>
-
-                                <View style={stylesItem.LinhaIcons}>
-
-                                    <View style={[stylesItem.IconLabel, {paddingTop: 10}]}>
-                                        <Image source={require('../../../assets/img/icons/home.png')} style={stylesItem.icon}/>
-                                        <Text style={stylesItem.label}>Quartos:Teste1</Text>
-                                    </View>
-
-                                    <View style={[stylesItem.IconLabel, {paddingBottom: 10}]}>
-                                        <Image source={require('../../../assets/img/icons/home.png')} style={stylesItem.icon}/>
-                                        <Text style={stylesItem.label}>Metros: Teste1</Text>
-                                    </View>
-
-                                </View>
-
-                                <View style={stylesItem.LinhaIcons}>
-
-                                    <View style={[stylesItem.IconLabel, {paddingTop: 10}]}>
-                                        <Image source={require('../../../assets/img/icons/home.png')} style={stylesItem.icon}/>
-                                        <Text style={stylesItem.label}>Vagas:Teste1</Text>
-                                    </View>
-
-                                    <View style={[stylesItem.IconLabel, {paddingBottom: 10}]}>
-                                        <Image source={require('../../../assets/img/icons/home.png')} style={stylesItem.icon}/>
-                                        <Text style={stylesItem.label}>Vagas Teste1</Text>
-                                    </View>
-
-                                </View>
+                                <Text style={stylesItem.txtBtn}>{this.props.data.creci}</Text>
+                                <Image source={require('../../../assets/img/icons/favorito.png')} style={stylesItem.icon}/>
 
                             </View>
 
+                            <View style={stylesItem.linha}>
+                            
+                                <Text style={stylesItem.txtBtn}>{this.props.data.creci}</Text>
+                                <Image source={require('../../../assets/img/icons/favorito.png')} style={stylesItem.icon}/>
+
+                            </View>
+
+                        </View>
                         </TouchableHighlight>
-                    </View>
-                
-                </View> 
+
+                </View>
         );
     }
 }
 
 const stylesItem = StyleSheet.create ({
-    viewItem:{
+    container:{
         backgroundColor:'white',
         marginTop:4,
         marginBottom:4,
@@ -217,46 +80,17 @@ const stylesItem = StyleSheet.create ({
         shadowRadius: 2,
         elevation: 1,
     },
-    viewTop:{
-        padding: 10,
+    linha:{
+        padding: 3,
         justifyContent:'space-between',
         flexDirection: 'row'
-    },
-    header:{
-       
-    },
-    viewImg:{
-        width:'100%',
-        height:200,
     },
     txtBtn:{
         fontWeight: 'bold',
         fontSize: 17,
     },
-    img:{
-        width:'100%',
-        height:200,
-    },
-    AreaItens:{
-        flexDirection:'row', 
-    },
-    LinhaIcons:{
-        width:'50%',
-    },
-    IconLabel:{
-        width:'100%',
-        flexDirection:'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        paddingLeft:25,
-        paddingTop:8,
-        paddingBottom:8,
-    },
-    label:{
-        paddingLeft:8,
-        fontSize: 14,
-        fontWeight: 'bold'
-    },
+
+    /*
     modalContent: {
         flex:1,
         backgroundColor: "white",
@@ -340,4 +174,5 @@ const stylesItem = StyleSheet.create ({
         left: 0,
         top: 0,
       },
+      */
 });
--- src/screens/default/Notificacoes.js
+++ src/screens/default/Notificacoes.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, FlatList, View, Text, ActivityIndicator, Button} from 'react-native';
+import {StyleSheet, FlatList, View, Text, ActivityIndicator, Switch} from 'react-native';
 import { connect } from 'react-redux';
 import NotificacoesItem from '../../Components/notificacoes/NotificacoesItem';
 import { NotificacoesList } from '../../actions/NotificacoesActions';
@@ -17,6 +17,7 @@ export class Notificacoes extends Component {
     this.state = { 
         loading: false,
         a: 'CARACA',
+        switch1Value: false,
     };
   
     this.props.NotificacoesList();
@@ -34,6 +35,11 @@ export class Notificacoes extends Component {
     houseClick(){
         
     }
+
+    toggleSwitch1 = (value) => {
+        this.setState({switch1Value: value})
+        console.log('Switch 1 is: ' + value)
+     }
 
     funcao(){
 
@@ -60,14 +66,41 @@ export class Notificacoes extends Component {
             </View>
             :
             <View >
-            <Text>CRECI {this.props.uid}</Text>
-            <Text>Status {this.props.status}</Text>
-            <FlatList 
-                data={this.props.lista}
-                windowSize={20}
-                renderItem={ ({item}) => <NotificacoesItem data={item} onPress={(this.houseClick)}/> } 
-            />
+
+                <View style={styles.topBar}>
+                
+                    <View style={styles.topBarLeft}>
+                        <Text style={styles.whiteText}>Nome e Sobrenome</Text>
+                        <Text style={styles.whiteText}>Creci: 000000-RO</Text>
+                    </View>
+
+                    <View style={styles.topBarRight}>
+                        <Text>Imagem</Text>
+                    </View>
+
+                </View>
+
+            <View style={styles.statusBar}>
+                
+                <View style={styles.statusBarLeft}>
+                    <Text> OFFLINE</Text>
+                </View>
+
+                <View style={styles.statusBarRight}>
+                    <Text>Ativar {this.props.status}</Text>
+                    <Switch
+                        onValueChange = {this.toggleSwitch1}
+                        value = {this.switch1Value}/>
+                </View>
+
             </View>
+            
+                <FlatList 
+                    data={this.props.lista}
+                    windowSize={20}
+                    renderItem={ ({item}) => <NotificacoesItem data={item} onPress={(this.houseClick)}/> } 
+                />
+        </View>
         } 
         </View>
     );
@@ -87,7 +120,37 @@ const styles = StyleSheet.create({
       flexDirection: 'row',
       justifyContent: 'space-around',
       padding: 10,
+      alignItems: 'center'
     },
+    topBar:{
+        backgroundColor:'#ACBEF1',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+    },
+    topBarLeft:{
+        padding: 10,
+    },
+    topBarRight:{
+        padding: 10,
+    },
+    whiteText:{
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 17,
+    },
+    statusBar:{
+        backgroundColor:'#FF5858',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+    },
+    statusBarLeft:{
+        padding: 10,
+    },
+    statusBarRight:{
+        flexDirection: 'row',
+        padding: 10,
+    },
+
 })
 const mapStateToProps = (state) => {
     return{
