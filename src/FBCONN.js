5187c6bb7b36f653cda9b3ac2f48efd43e37b624
Naamã Ortiz <naama_ortiz@hotmail.com>
2018-12-16 15:48:38 -0400

    blockvirgin

--- src/FBCONN.js
+++ src/FBCONN.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
   // Initialize Firebase
   let config = {
-    apiKey: "AIzaSyCSlfrBnrbJmQClXC410VhpOvPpY28iEpc",
-    authDomain: "appimob-f723e.firebaseapp.com",
-    databaseURL: "https://appimob-f723e.firebaseio.com",
-    projectId: "appimob-f723e",
-    storageBucket: "appimob-f723e.appspot.com",
-    messagingSenderId: "426777779766"
+    apiKey: "xxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxx"
   };
   firebase.initializeApp(config);
 
